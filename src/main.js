ffba68ab68a0c36db0b0dc36c4c7f5756ba6d873
Nik <shevchenko.nik@icloud.com>
2024-10-01 13:06:36 +0200

    Added Haarlem

--- src/main.js
+++ src/main.js
@@ -17,7 +17,8 @@ const { CHAT_ID, BOT_API } = process.env;
 
 const urls = [
     // 'https://www.funda.nl/en/huur/amsterdam/beschikbaar/0-2000/2+kamers/1-dag/+15km/',
-    'https://www.funda.nl/en/zoeken/huur?selected_area=%5B%22amsterdam,15km%22%5D&rooms=%222-4%22&publication_date=%221%22&availability=%5B%22available%22%5D&object_type=%5B%22apartment%22%5D&price=%22-2300%22'
+    'https://www.funda.nl/en/zoeken/huur?selected_area=%5B%22amsterdam,15km%22%5D&rooms=%222-4%22&publication_date=%221%22&availability=%5B%22available%22%5D&object_type=%5B%22apartment%22%5D&price=%22-2300%22',
+    'https://www.funda.nl/en/zoeken/huur?selected_area=%5B%22haarlem,5km%22%5D&price=%22-2100%22&rooms=%222-%22&publication_date=%223%22&availability=%5B%22available%22%5D'
 ];
 
 const runTask = async () => {
